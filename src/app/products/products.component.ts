ce7ef3f13953ca64197a4151499355f37ac62dea
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2017-10-25 17:17:58 +0530

    Initial commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+ pageTitle:string='Product List'
+ products:any[]=[
+   {
+     "productId":2,
+     "productName":"Garden Cart",
+     "productCode":"GDN-0023",
+     "releaseDate":"March 18,2017",
+     "price":32.20,
+     "star":4.2,
+     "url":"https://openclipart.org/download/58471/garden-cart.svg"
+   },
+   {
+    "productId":3,
+    "productName":"Hammer",
+    "productCode":"TBX-0023",
+    "releaseDate":"March 21,2017",
+    "price":8.9,
+    "star":4.8,
+    "url":"https://openclipart.org/download/193438/freehammer.svg"
+  }
+ ]
+}
