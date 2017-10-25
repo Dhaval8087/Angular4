a4f8151024bddd2f250805aa4957219b39fdd80a
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2017-10-25 17:40:46 +0530

    added button image

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,25 +11,32 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
   }
- pageTitle:string='Product List'
- products:any[]=[
-   {
-     "productId":2,
-     "productName":"Garden Cart",
-     "productCode":"GDN-0023",
-     "releaseDate":"March 18,2017",
-     "price":32.20,
-     "star":4.2,
-     "url":"https://openclipart.org/download/58471/garden-cart.svg"
-   },
-   {
-    "productId":3,
-    "productName":"Hammer",
-    "productCode":"TBX-0023",
-    "releaseDate":"March 21,2017",
-    "price":8.9,
-    "star":4.8,
-    "url":"https://openclipart.org/download/193438/freehammer.svg"
+  pageTitle: string = 'Product List'
+  imagewidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18,2017",
+      "price": 32.20,
+      "star": 4.2,
+      "url": "https://openclipart.org/download/58471/garden-cart.svg"
+    },
+    {
+      "productId": 3,
+      "productName": "Hammer",
+      "productCode": "TBX-0023",
+      "releaseDate": "March 21,2017",
+      "price": 8.9,
+      "star": 4.8,
+      "url": "https://openclipart.org/download/193438/freehammer.svg"
+    }
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
   }
- ]
 }
