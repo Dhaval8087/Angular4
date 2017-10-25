c768dadcac035afcb214b824c33e5350f896b133
LAPTOP-P2JECQC3\Dhaval <erdhavalpatel@yahoo.com>
2017-10-25 23:24:09 +0530

    added filter supports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
-
+import {ConvertToSpacePipe} from './shared/convert-to-spaces.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ConvertToSpacePipe
   ],
   imports: [
     BrowserModule,
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IProduct } from './product';
 @Component({
   selector: 'pm-products',
   templateUrl: './products.component.html',
@@ -7,17 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.filterProducts=this.products;
+    this.listFilter='cart';
+   }
 
   ngOnInit() {
+
   }
   pageTitle: string = 'Product List'
   imagewidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = '';
+  _listFilter = '';
   currencyCode: string = '$';
-  products: any[] = [
+  filterProducts: IProduct[];
+  products: IProduct[] = [
     {
       "productId": 2,
       "productName": "Garden Cart",
@@ -37,8 +42,18 @@ export class ProductsComponent implements OnInit {
       "url": "https://openclipart.org/download/193438/freehammer.svg"
     }
   ];
-
+  get listFilter() {
+    return this._listFilter;
+  }
+  set listFilter(value) {
+    this._listFilter = value;
+    this.filterProducts = value ? this.performedFilter(value) : this.products;
+  }
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+  performedFilter(value) {
+    value = value.toLowerCase();
+    return this.products.filter((product) => product.productName.toLocaleLowerCase().indexOf(value) !== -1);
   }
 }
